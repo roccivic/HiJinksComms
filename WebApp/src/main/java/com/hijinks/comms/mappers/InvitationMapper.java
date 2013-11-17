c9a8e6b8804a2a34ce853b055751938077039cc2
Preslav Petkov <pres.petkov@gmail.com>
2013-11-17 14:36:43 +0000

    Fixed Invitation and Request Mappers

--- WebApp/src/main/java/com/hijinks/comms/mappers/InvitationMapper.java
+++ WebApp/src/main/java/com/hijinks/comms/mappers/InvitationMapper.java
@@ -2,16 +2,26 @@ package com.hijinks.comms.mappers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import org.springframework.jdbc.core.RowMapper;
+
+import com.hijinks.comms.models.Community;
 import com.hijinks.comms.models.Invitation;
+import com.hijinks.comms.models.User;
 
 public class InvitationMapper implements RowMapper<Invitation>{
 	   public Invitation mapRow(ResultSet rs, int rowNum) throws SQLException {
+		   Community community = new Community();
+		   //TO DO
+		   User invitedBy = new User();
+		   //TO DO
+		   User invitee = new User();
+		   //TO DO
 		   Invitation invitation = new Invitation();
 		   invitation.setId(rs.getInt("id"));
-		   invitation.setCommunity(rs.getInt("communityId"));
-		   invitation.setInvitedBy(rs.getInt("invitedBy"));
-		   invitation.setInvitee(rs.getInt("invitee"));
+		   invitation.setCommunity(community);
+		   invitation.setInvitedBy(invitedBy);
+		   invitation.setInvitee(invitee);
 		   invitation.setDate(rs.getTimestamp("date"));
 	      return invitation;
 	   }
--- WebApp/src/main/java/com/hijinks/comms/mappers/RequestMapper.java
+++ WebApp/src/main/java/com/hijinks/comms/mappers/RequestMapper.java
@@ -2,15 +2,23 @@ package com.hijinks.comms.mappers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import org.springframework.jdbc.core.RowMapper;
+
+import com.hijinks.comms.models.Community;
 import com.hijinks.comms.models.Request;
+import com.hijinks.comms.models.User;
 
 public class RequestMapper implements RowMapper<Request>{
 	   public Request mapRow(ResultSet rs, int rowNum) throws SQLException {
+		   Community community = new Community();
+		   //TO DO
+		   User user = new User();
+		   //TO DO
 		   Request request = new Request();
 		   request.setId(rs.getInt("id"));
-		   request.setCommunity(rs.getInt("community"));
-		   request.setUser(rs.getInt("user"));
+		   request.setCommunity(community);
+		   request.setUser(user);
 		   request.setDate(rs.getTimestamp("date"));
 		   return request;
 	   }
