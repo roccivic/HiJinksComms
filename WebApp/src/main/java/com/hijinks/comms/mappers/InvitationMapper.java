7daddfe160d2de4793bf5e84e0c72a3f886d68f9
Preslav Petkov <pres.petkov@gmail.com>
2013-11-17 15:14:12 +0000

    Corrected InvitationJDBC Template

--- WebApp/src/main/java/com/hijinks/comms/mappers/InvitationMapper.java
+++ WebApp/src/main/java/com/hijinks/comms/mappers/InvitationMapper.java
@@ -11,12 +11,34 @@ import com.hijinks.comms.models.User;
 
 public class InvitationMapper implements RowMapper<Invitation>{
 	   public Invitation mapRow(ResultSet rs, int rowNum) throws SQLException {
+		   User communityOwner = new User();
+		   communityOwner.setId(rs.getInt("communityOwnerId"));
+		   communityOwner.setFname(rs.getString("communityOwnerFname"));
+		   communityOwner.setLname(rs.getString("communityOwnerLname"));
+		   communityOwner.setEmail(rs.getString("communityOwnerEmail"));
+		   communityOwner.setType(rs.getString("communityOwnerType"));
 		   Community community = new Community();
-		   //TO DO
+		   community.setId(rs.getInt("communityId"));
+		   community.setName(rs.getString("name"));
+		   community.setOwner(communityOwner);
+		   community.setDescription(rs.getString("description"));
+		   community.setKeywords(rs.getString("keywords"));
+		   community.setKeywordsEnabled(rs.getBoolean("keywordsEnabled"));
+		   community.setAccessLevel(rs.getString("accessLevel"));
+		   community.setVisibilityLevel(rs.getString("visibilityLevel"));
+		   community.setCreated(rs.getTimestamp("created"));
 		   User invitedBy = new User();
-		   //TO DO
+		   invitedBy.setId(rs.getInt("invitedById"));
+		   invitedBy.setFname(rs.getString("invitedByFname"));
+		   invitedBy.setLname(rs.getString("invitedByLname"));
+		   invitedBy.setEmail(rs.getString("invitedByEmail"));
+		   invitedBy.setType(rs.getString("invitedByType"));
 		   User invitee = new User();
-		   //TO DO
+		   invitedBy.setId(rs.getInt("inviteeId"));
+		   invitedBy.setFname(rs.getString("inviteeFname"));
+		   invitedBy.setLname(rs.getString("inviteeLname"));
+		   invitedBy.setEmail(rs.getString("inviteeEmail"));
+		   invitedBy.setType(rs.getString("inviteeType"));
 		   Invitation invitation = new Invitation();
 		   invitation.setId(rs.getInt("id"));
 		   invitation.setCommunity(community);
