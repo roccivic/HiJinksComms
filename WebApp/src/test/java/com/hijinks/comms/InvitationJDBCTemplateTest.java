cb8b7b651228c5fe9a662fdb7df7b40532ebd970
Preslav Petkov <pres.petkov@gmail.com>
2013-11-17 00:14:15 +0000

    Added Tests for InvitationJDBCTemplate

---
+++ WebApp/src/test/java/com/hijinks/comms/InvitationJDBCTemplateTest.java
@@ -0,0 +1,34 @@
+package com.hijinks.comms;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hijinks.comms.dao.impl.InvitationJDBCTemplate;
+import com.hijinks.comms.models.Invitation;
+
+public class InvitationJDBCTemplateTest {
+	public static void main(String[] args) {
+		ApplicationContext context = 
+	             new ClassPathXmlApplicationContext("config.xml");
+
+		InvitationJDBCTemplate invitationJDBCTemplate = 
+	      (InvitationJDBCTemplate)context.getBean("invitationJDBCTemplate");
+		
+		//create invite
+		invitationJDBCTemplate.sendInvite(1, 19, 2);
+		System.out.println("Created invite");
+		//read invites
+		List<Invitation> invitations = invitationJDBCTemplate.getInvites(2);
+		for (Invitation record : invitations) {
+	         System.out.println("ID : " + record.getId());
+	         System.out.println("Invitee : " + record.getInvitee());
+	         System.out.println("Comunity Id : " + record.getCommunity());
+	         System.out.println("Invited By : " + record.getInvitedBy());
+	         System.out.println("Date : " + record.getDate());
+	         System.out.println("--------------------");
+	      }
+		//remove invite
+		invitationJDBCTemplate.deleteInvite(2, 19);
+	}
+}
